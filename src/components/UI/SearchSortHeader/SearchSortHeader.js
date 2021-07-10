865502cd6f97005f0ea05a0b7fb0e4d2b29fc07e
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-07-10 22:18:35 +0700

    sort article fixed

--- src/components/UI/SearchSortHeader/SearchSortHeader.js
+++ src/components/UI/SearchSortHeader/SearchSortHeader.js
@@ -24,6 +24,7 @@ const SearchSortHeader = (props) => {
         setSortType("Oldest");
         history.push(searchPath);
     }
+    
     return (
         <div className="bg-dark text-white p-2 d-flex align-items-center" >
             {props.searchText ? <p className="mr-auto mb-0">Result for <strong>{props.searchText}</strong></p> : <div className="mr-auto"></div>}
